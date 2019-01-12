a7d172fe0d0a2fdcb93528e4bcb7cac9c3e72e0f
MCMrARM <mcmrarm@gmail.com>
2019-01-12 22:17:07 +0100

    Inital commit

---
+++ lib/src/main/java/io/mrarm/thememonkey/Theme.java
@@ -0,0 +1,37 @@
+package io.mrarm.thememonkey;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.res.AssetManager;
+import android.os.Build;
+
+public class Theme {
+
+    private static AssetManager createAssetManager(Context context, String themeFile) {
+        AssetManager assetManager = AssetManagerReflectionHelper.create();
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        AssetManagerReflectionHelper.addAssetPath(assetManager, applicationInfo.sourceDir);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            for (String path : applicationInfo.splitSourceDirs)
+                AssetManagerReflectionHelper.addAssetPath(assetManager, path);
+        }
+        AssetManagerReflectionHelper.addAssetPath(assetManager, themeFile);
+        // required on pre-KitKat according to Google in their MonkeyPatcher code
+        AssetManagerReflectionHelper.ensureStringBlocks(assetManager);
+        return assetManager;
+    }
+
+
+    private AssetManager assetManager;
+
+    public Theme(Context context, String themeFile) {
+        assetManager = createAssetManager(context, themeFile);
+    }
+
+    public void applyToActivity(Activity activity) {
+        MonkeyPatcher.setActivityAssetManager(activity, assetManager);
+        MonkeyPatcher.updateResourcesConfiguration(activity.getResources());
+    }
+
+}
